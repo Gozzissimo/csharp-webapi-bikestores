fa7bcbffcfc5681c02d8268a93063e60721723e9
Nicolo Fantini <nicolo.fantini@plug-in.it>
2022-10-27 11:27:13 +0200

    Show Orders - In progress

--- project.frontend/ClientApp/src/app/orders/show-order/show-order.component.ts
+++ project.frontend/ClientApp/src/app/orders/show-order/show-order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { concat, of } from 'rxjs';
+import { Order } from '../../dto/Order.interface';
+import { OrderItemService } from '../../services/order-item.service';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-show-order',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowOrderComponent implements OnInit {
 
-  constructor() { }
+  orderId!: number;
+  actualOrder!: Order;
+  isLoading: boolean = true;
+  id: number = 0;
 
-  ngOnInit(): void {
+  constructor(
+    private _Activatedroute: ActivatedRoute,
+    private orderService: OrderService,
+    private orderItemService: OrderItemService,
+    private router: Router
+  ) {
   }
 
+  GetDataByParam() {
+    concat(
+      this.orderId = this._Activatedroute.snapshot.params["orderId"],
+      of(this.orderService
+        .FindByIdAsync(this.orderId)
+        .subscribe(
+          (res) => {
+            this.actualOrder = res
+            this.isLoading = false
+          }
+        ))
+    )
+  }
+
+  ngOnInit(): void {
+    this.GetDataByParam()
+  }
+
+  //FUNZIONE DELETE
+  loading: boolean = false;
+  errorMessage!: string;
+
+  public DeleteAsync(id: number) {
+    this.loading = true;
+    this.errorMessage = "";
+    if (confirm("Are you sure do you want to delete this Product?")) {
+      this.orderService.DeleteAsync(id)
+        .subscribe(
+          (response) => {                           //next() callback
+            this.router.navigate(['/products']);
+          },
+          (error) => {                              //error() callback
+            console.error('Request failed with error')
+            this.errorMessage = error;
+            this.loading = false;
+          },
+          () => {                                   //complete() callback
+            this.loading = false;
+          })
+    }
+  }
 }
