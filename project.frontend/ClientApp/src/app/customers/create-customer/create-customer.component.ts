52d2340175b17ae9fd6898e5bf0e920eb9a2b686
Nicolo Fantini <nicolo.fantini@plug-in.it>
2022-10-28 15:42:00 +0200

    Create Customer

--- project.frontend/ClientApp/src/app/customers/create-customer/create-customer.component.ts
+++ project.frontend/ClientApp/src/app/customers/create-customer/create-customer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Customer } from '../../dto/Customer.interface';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-create-customer',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCustomerComponent implements OnInit {
 
-  constructor() { }
+  public filterText: string = '';
+  public loading: boolean = false;
+  public errorMessage!: string;
 
-  ngOnInit(): void {
+  //BUILDER DEL FORM
+  customerForm: FormGroup = this.formBuilder.group({
+    firstName: [null],
+    lastName: [null],
+    phone: [null],
+    email: [null],
+    street: [null],
+    city: [null],
+    state: [null],
+    zipCode: [null]
+  })
+
+  constructor(
+    private customerService: CustomerService,
+    private router: Router,
+    public formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
   }
 
+  //FUNZIONE PER CREARE L'OGGETTO
+  public CreateAsync() {
+
+    this.loading = true;
+    this.errorMessage = "";
+    console.log(this.customerForm.value);
+    this.customerService.CreateAsync(<Customer>this.customerForm.value)
+      .subscribe(
+        (response) => {                           //next() callback
+          this.router.navigate(['/customers']);
+        },
+        (error) => {                              //error() callback
+          console.error('Request failed with error')
+          this.errorMessage = error;
+          this.loading = false;
+        },
+        () => {                                   //complete() callback
+          console.info('Request completed')      //This is actually not needed 
+          this.loading = false;
+        })
+  }
 }
