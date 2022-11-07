cad9852224794d7712fecdca10e5e8e8291efb0d
Nicolo Fantini <nicolo.fantini@plug-in.it>
2022-11-07 10:53:39 +0100

    JWT Authentication - Funzionante, da fare refactoring

--- project.webapi/Controllers/UserController.cs
+++ project.webapi/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using project.data;
 using project.dtos;
 using project.workers;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace project.webapi.Controllers
@@ -11,9 +14,14 @@ namespace project.webapi.Controllers
     public class UserController : ControllerBase
     {
         private readonly IWorker<User> _worker;
-        public UserController(IWorker<User> worker)
+        private readonly IConfiguration _configuration;
+
+        public UserController(
+            IWorker<User> worker,
+            IConfiguration configuration)
         {
             this._worker = worker;
+            _configuration = configuration;
         }
 
         public static User user = new User();
@@ -39,8 +47,38 @@ namespace project.webapi.Controllers
             {
                 return BadRequest("User not found.");
             }
-            return Ok();
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong Password");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
         }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             var hmac = new HMACSHA512();
@@ -50,7 +88,7 @@ namespace project.webapi.Controllers
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            var hmac = new HMACSHA512(user.PasswordSalt);
+            var hmac = new HMACSHA512(passwordSalt);
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return computedHash.SequenceEqual(passwordHash);
