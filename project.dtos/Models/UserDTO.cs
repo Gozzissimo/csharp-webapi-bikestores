766ba3d4eeabcc5b487377f6b564ffe3ed6b7f60
Nicolo Fantini <nicolo.fantini@plug-in.it>
2022-11-07 17:42:19 +0100

    Added UserId

--- project.dtos/Models/UserDTO.cs
+++ project.dtos/Models/UserDTO.cs
@@ -2,6 +2,7 @@
 {
     public partial class UserDTO
     {
+        public int UserId { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
